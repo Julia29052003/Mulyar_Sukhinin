2d19ec4d708c0f0b3d75e125232a2f8a53702a2a
Student <pochta@mail.ru>
2022-05-17 12:18:00 +0500

    Second commit

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class TestClass
+    {
+        int x = 1;
+        int y = 1;
+
+        void OutputInfo()
+        {
+            Console.WriteLine(Convert.ToString(x) + "" + Convert.ToString(y));
+        }
+
+        int XY()
+        {
+            for (int i = 0; i < y; i++)
+            {
+                x = x * x;
+
+                Console.WriteLine(Convert.ToString(x));
+            }
+        }
+    }
+}
